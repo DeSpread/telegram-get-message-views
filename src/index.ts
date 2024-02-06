935ea7017033f7d08b98e1388d2f62420b01f7e8
LivetOwO <livet@kakao.com>
2024-02-06 23:14:49 +0900

    setup dotenv for bot token

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello world!");
+import * as dotenv from "dotenv";
+
+dotenv.config({
+  path: [".env.local", ".env"],
+});
+
+const botToken = process.env.TELEGRAM_BOT_TOKEN;
+
+console.log("Hello world!", botToken);
